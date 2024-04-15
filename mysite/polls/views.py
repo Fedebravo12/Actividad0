318ba591119390582c2fcc1879e6e4b2dc13b665
Diego Federico Bravo <fedebravo2016@gmail.com>
2024-04-14 21:03:08 -0300

    Parte 3.3 genero un error 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.template import loader
+from .models import Question
 from django.http import HttpResponse
 
 
@@ -18,15 +20,13 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-from django.http import HttpResponse
-from django.template import loader
-
-from .models import Question
-
-
-
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
